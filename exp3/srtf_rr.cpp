28bb1de8cd8d0a2750eee633a15e14b46365b014
Chinmay Joshi <chinmayujoshi@gmail.com>
2022-10-09 06:47:25 +0530

    exp3 complete

--- exp3/srtf_rr.cpp
+++ exp3/srtf_rr.cpp
@@ -4,7 +4,6 @@
 
 #include<iostream>
 #include<iomanip>
-#include<string>
 
 #define infinity 9999;
 
@@ -33,7 +32,6 @@ int main() {
         cin>>priority[i];
     }
 
-
     // SRTF
     bool* completion_record = new bool[n]; //true if a process has completed execution
     bool* arrival_status = new bool[n]; // true is a process has already arrived
@@ -53,9 +51,6 @@ int main() {
     int process_with_min_remaining_burst_time = 0;
 x:  for (int i = 1; i <= n; i++) if (cpu_time >= arrival_time[i]) arrival_status[i] = true;
     int min_remaining_burst_time = infinity;
-
-/*****************************************************************
-
     for (int i = n; i >= 1; i--) {
         // finding process with min remaining burst time that is alaready arrived
         if (arrival_status[i] == true && completion_record[i] != true) {
@@ -100,6 +95,7 @@ x:  for (int i = 1; i <= n; i++) if (cpu_time >= arrival_time[i]) arrival_status
         wait_time[currently_exec_process] = completion_time[currently_exec_process] - burst_time[currently_exec_process] - arrival_time[currently_exec_process];
     }
     previously_exec_process = currently_exec_process;
+    flag=0;
     count++;
     // looping until all processes are completed
     for (int i = 1; i <= n; i++) if (completion_record[i] != true) goto x;
@@ -135,11 +131,10 @@ x:  for (int i = 1; i <= n; i++) if (cpu_time >= arrival_time[i]) arrival_status
     cout << endl << endl;
     cout << "Average wait time for the processes using SRTF algorithm = " << srtf_avg_wait_and_tat[0];
     cout << endl << "Average turnaround time for the processes using SRTF algorithm= " << srtf_avg_wait_and_tat[1] << endl;
-  
-  *******************************************************************/
+
     // RR
     int time_quantum;
-    cout<<"\nFor Round Robin algorithm, enter time quantunm: ";
+    cout<<"\nFor Round Robin algorithm, enter time quantum: ";
     cin>>time_quantum;
     completion_record = new bool[n]; //true if a process has completed execution
     arrival_status = new bool[n]; // true is a process has already arrived
@@ -156,19 +151,13 @@ x:  for (int i = 1; i <= n; i++) if (cpu_time >= arrival_time[i]) arrival_status
     cpu_time = 0;
     count = 0; // count of executed processes
     flag = 0;
-  //  process_with_min_remaining_burst_time = 0;
 x1:  for (int i = 1; i <= n; i++) if (cpu_time >= arrival_time[i]) arrival_status[i] = true;
-    min_remaining_burst_time = infinity;
     int highest_priority=infinity;
     for (int i = n; i >= 1; i--) {
-        // finding process with that is alaready arrived and has high priority
+        // finding process with that is already arrived and has high priority
         if (arrival_status[i] == true && completion_record[i] != true) {
-            if (priority[i] < highest_priority) {
+            if (priority[i] <= highest_priority) {
                 highest_priority = priority[i];
-                currently_exec_process = i;
-                flag = 1;
-            }
-            else if (priority[i] == highest_priority) {
                 currently_exec_process = i;
                 flag = 1;
             }
@@ -184,30 +173,39 @@ x1:  for (int i = 1; i <= n; i++) if (cpu_time >= arrival_time[i]) arrival_statu
     // if not,update cpu_time to the arrival time of the first process
     if (count == 0 && arrival_time[currently_exec_process] != 0) cpu_time = arrival_time[currently_exec_process];
     // check if the process is executing for the first time
-    if (burst_time[currently_exec_process] == remaining_burst_time[currently_exec_process]) {
-        wait_time[currently_exec_process] = cpu_time - arrival_time[currently_exec_process];
-    }
+    if (burst_time[currently_exec_process] == remaining_burst_time[currently_exec_process]) wait_time[currently_exec_process] = cpu_time - arrival_time[currently_exec_process];
+    if (currently_exec_process != previously_exec_process) start_time[currently_exec_process] = cpu_time;
+    if (start_time[currently_exec_process] >= 0) wait_time[currently_exec_process] += (cpu_time - burst_time[currently_exec_process] + remaining_burst_time[currently_exec_process]);
     if(remaining_burst_time[currently_exec_process]>=time_quantum){
         cpu_time+=time_quantum;
-        remaining_burst_time[currently_exec_process]=remaining_burst_time[currently_exec_process]-time_quantum;
+        remaining_burst_time[currently_exec_process]-=time_quantum;
+    }
+    else if(remaining_burst_time[currently_exec_process]<time_quantum){
+        cpu_time+=remaining_burst_time[currently_exec_process];
+        remaining_burst_time[currently_exec_process]=0;
+    }
+    if(remaining_burst_time[currently_exec_process]==0) 
+    {
+        completion_record[currently_exec_process]=true;
+        completion_time[currently_exec_process]=cpu_time;
+        turnaround_time[currently_exec_process] = completion_time[currently_exec_process] - arrival_time[currently_exec_process];
+        wait_time[currently_exec_process] = completion_time[currently_exec_process] - burst_time[currently_exec_process] - arrival_time[currently_exec_process];
     }
     count++;
+    flag=0;
+    previously_exec_process = currently_exec_process;
     // looping until all processes are completed
     for (int i = 1; i <= n; i++) if (completion_record[i] != true) goto x1;
     // displaying results in tabular form
-
-      const char space = ' ';
-    const int width = 17;
     cout << endl << "**** Scheduling according to RR algorithm ****" << endl;
+    cout<<endl<<"Time Quantum = "<<time_quantum<<endl;
     cout << endl << left << setw(width) << setfill(space) << "process_id";
     cout << left << setw(width) << setfill(space) << "arrival_time";
     cout << left << setw(width) << setfill(space) << "burst_time";
     cout << left << setw(width) << setfill(space) << "priority";
     cout << left << setw(width) << setfill(space) << "completion_time";
     cout << left << setw(width) << setfill(space) << "wait_time";
-    // cout << left << setw(width) << setfill(space) << "turnaround_time";
-        cout << left << setw(width) << setfill(space) << "remBtime";
-
+    cout << left << setw(width) << setfill(space) << "turnaround_time";
     for (int i = 1; i <= n; i++) {
         cout << endl;
         cout << left << setw(width) << setfill(space) << process_id[i];
@@ -216,13 +214,11 @@ x1:  for (int i = 1; i <= n; i++) if (cpu_time >= arrival_time[i]) arrival_statu
         cout << left << setw(width) << setfill(space) << priority[i];
         cout << left << setw(width) << setfill(space) << completion_time[i];
         cout << left << setw(width) << setfill(space) << wait_time[i];
-        // cout << left << setw(width) << setfill(space) << turnaround_time[i];
-                cout << left << setw(width) << setfill(space) << remaining_burst_time[i];
-
+        cout << left << setw(width) << setfill(space) << turnaround_time[i];
     }
     // final results of RR
-    int  wait_time_sum = 0;
-    int  turnaround_time_sum = 0;
+    wait_time_sum = 0;
+    turnaround_time_sum = 0;
     for (int i = 1; i <= n; i++) {
         wait_time_sum += wait_time[i];
         turnaround_time_sum += turnaround_time[i];
@@ -231,39 +227,96 @@ x1:  for (int i = 1; i <= n; i++) if (cpu_time >= arrival_time[i]) arrival_statu
     cout << endl << endl;
     cout << "Average wait time for the processes using RR algorithm = " << rr_avg_wait_and_tat[0];
     cout << endl << "Average turnaround time for the processes using RR algorithm= " << rr_avg_wait_and_tat[1] << endl;
-   
-    //int p;
-    //cin >> p;
+    // final comparison
+    cout<<endl<<"**** Comparison between SRTF and RR ****"<<endl;
+    cout<<endl<<left<<setw(width)<<setfill(space)<<"algorithm";
+    cout<<left<<setw(width)<<setfill(space)<<"avg_wait_time";
+    cout<<left<<setw(width)<<setfill(space)<<"avg_turnaround_time";
+    cout<<endl;
+    cout<<left<<setw(width)<<setfill(space)<<"SRTF";
+    cout<<left<<setw(width)<<setfill(space)<<srtf_avg_wait_and_tat[0];
+    cout<<left<<setw(width)<<setfill(space)<<srtf_avg_wait_and_tat[1];
+    cout<<endl;
+    cout<<left<<setw(width)<<setfill(space)<<"RR";
+    cout<<left<<setw(width)<<setfill(space)<<rr_avg_wait_and_tat[0];
+    cout<<left<<setw(width)<<setfill(space)<<rr_avg_wait_and_tat[1];
+    cout<<endl<<endl;
 }
 /* Sample output
-Enter number of processes:5                                                                                                                                                                                                                     
-Enter process id for process #1 : 1001                                                                                                                                                                                                          
-Enter arrival time for process #1 (process_id:1001): 1                                                                                                                                                                                          
-Enter burst time for process #1 (process_id:1001): 8                                                                                                                                                                                            
-Enter priority for process #1 (process_id:1001)[lower number higher priority]: 4                                                                                                                                                                
-Enter process id for process #2 : 1002                                                                                                                                                                                                          
-Enter arrival time for process #2 (process_id:1002): 2                                                                                                                                                                                          
-Enter burst time for process #2 (process_id:1002): 6                                                                                                                                                                                            
-Enter priority for process #2 (process_id:1002)[lower number higher priority]: 1                                                                                                                                                                
-Enter process id for process #3 : 1003                                                                                                                                                                                                          
-Enter arrival time for process #3 (process_id:1003): 5                                                                                                                                                                                          
-Enter burst time for process #3 (process_id:1003): 1                                                                                                                                                                                            
-Enter priority for process #3 (process_id:1003)[lower number higher priority]: 2                                                                                                                                                                
-Enter process id for process #4 : 1004                                                                                                                                                                                                          
-Enter arrival time for process #4 (process_id:1004): 4                                                                                                                                                                                          
-Enter burst time for process #4 (process_id:1004): 9                                                                                                                                                                                            
-Enter priority for process #4 (process_id:1004)[lower number higher priority]: 2                                                                                                                                                                
-Enter process id for process #5 : 1005                                                                                                                                                                                                          
-Enter arrival time for process #5 (process_id:1005): 3                                                                                                                                                                                          
-Enter burst time for process #5 (process_id:1005): 3                                                                                                                                                                                            
-Enter priority for process #5 (process_id:1005)[lower number higher priority]: 3                                                                                                                                                                
-**** Scheduling according to SRTF algorithm ****                                                                                                                                                                                                
-process_id       arrival_time     burst_time       priority         completion_time  wait_time        turnaround_time   
-1001             1                8                4                19               10               18                
-1002             2                6                1                12               4                10                
-1003             5                1                2                6                0                1                 
-1004             4                9                2                28               15               24                
-1005             3                3                3                7                1                4                                                                                                                                         
-Average wait time for the processes using SRTF algorithm = 6                                                            
+
+Enter number of processes:5
+
+Enter process id for process #1 : 1001
+
+Enter arrival time for process #1 (process_id:1001): 1
+
+Enter burst time for process #1 (process_id:1001): 8
+
+Enter priority for process #1 (process_id:1001)[lower number higher priority]: 4
+
+Enter process id for process #2 : 1002
+
+Enter arrival time for process #2 (process_id:1002): 2
+
+Enter burst time for process #2 (process_id:1002): 6
+
+Enter priority for process #2 (process_id:1002)[lower number higher priority]: 1
+
+Enter process id for process #3 : 1003
+
+Enter arrival time for process #3 (process_id:1003): 5
+
+Enter burst time for process #3 (process_id:1003): 1
+
+Enter priority for process #3 (process_id:1003)[lower number higher priority]: 2
+
+Enter process id for process #4 : 1004
+
+Enter arrival time for process #4 (process_id:1004): 4
+
+Enter burst time for process #4 (process_id:1004): 9
+
+Enter priority for process #4 (process_id:1004)[lower number higher priority]: 2
+
+Enter process id for process #5 : 1005
+
+Enter arrival time for process #5 (process_id:1005): 3
+
+Enter burst time for process #5 (process_id:1005): 3
+
+Enter priority for process #5 (process_id:1005)[lower number higher priority]: 3
+
+**** Scheduling according to SRTF algorithm ****
+
+process_id       arrival_time     burst_time       priority         completion_time  wait_time        turnaround_time  
+1001             1                8                4                19               10               18               
+1002             2                6                1                12               4                10               
+1003             5                1                2                6                0                1                
+1004             4                9                2                28               15               24               
+1005             3                3                3                7                1                4                
+
+Average wait time for the processes using SRTF algorithm = 6
 Average turnaround time for the processes using SRTF algorithm= 11.4
+
+For Round Robin algorithm, enter time quantunm: 2
+
+**** Scheduling according to RR algorithm ****
+
+Time Quantum = 2
+
+process_id       arrival_time     burst_time       priority         completion_time  wait_time        turnaround_time  
+1001             1                8                4                28               19               27               
+1002             2                6                1                9                1                7                
+1003             5                1                2                10               4                5                
+1004             4                9                2                19               6                15               
+1005             3                3                3                22               16               19               
+
+Average wait time for the processes using RR algorithm = 9.2
+Average turnaround time for the processes using RR algorithm= 14.6
+
+**** Comparison between SRTF and RR ****
+
+algorithm        avg_wait_time    avg_turnaround_time
+SRTF             6                11.4             
+RR               9.2              14.6 
 */
